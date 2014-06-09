cdc2781ef0f56f2ee8afbf7446b615b315e065b4
klim <git@leckstrom.de>
2014-06-09 16:34:18 +0200

    add correct email address

--- core/ConfigHandler.py
+++ core/ConfigHandler.py
@@ -2,7 +2,7 @@
 
 ##############################################################################
 #    Copyright (C) 2013
-#    by Klemens Fritzsche, klim@leckstrom.de
+#    by Klemens Fritzsche, pyplane@leckstrom.de
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
