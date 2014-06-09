218f1c15e10bafbc3b6c8e5b2286590987efa9c8
klim <git@leckstrom.de>
2014-06-09 16:33:55 +0200

    add correct email address

--- core/FunctionHandler.py
+++ core/FunctionHandler.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ##############################################################################
-#    Copyright (C) 2013  Klemens Fritzsche, klim@leckstrom.de
+#    Copyright (C) 2013  Klemens Fritzsche, pyplane@leckstrom.de
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
