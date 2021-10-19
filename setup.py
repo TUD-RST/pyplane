d2b8b68ba85d829a33d10c4a3d44af8000b94bc7
Shane Allcroft <shaneallcroft@gmail.com>
2021-10-19 09:55:28 -0400

    reverting setup.py for simplicity

--- setup.py
+++ setup.py
@@ -1,40 +1,21 @@
-import os
-import platform
 from setuptools import setup, find_packages
 
 with open("requirements.txt") as requirements_file:
     requirements = requirements_file.read()
 
 
-if platform.system() == 'Linux':    
-    setup(
-        name='PyPlane',
-        version='2.0.0',
-        license='GPLv3',
-        url='https://github.com/TUD-RST/pyplane',
-        author='Klemens Fritzsche, Jan Winkler, Carsten Knoll',
-        author_email='firstname.lastname@tu-dresden.de',
-        description='Tool for phase plane analysis of second order dynamical systems (Qt-based)',
-        packages=find_packages(),
-        include_package_data=True,
-        install_requires=requirements,
-        entry_points={'console_scripts': ['pyplane=pyplane.app:run']},
-        data_files=['packaging/linux/pyplane.desktop',
-                    'packaging/linux/pyplane.metainfo.xml'
-        ]
-    )
-else:
-    setup(
-        name='PyPlane',
-        version='2.0.0',
-        license='GPLv3',
-        url='https://github.com/TUD-RST/pyplane',
-        author='Klemens Fritzsche, Jan Winkler, Carsten Knoll',
-        author_email='firstname.lastname@tu-dresden.de',
-        description='Tool for phase plane analysis of second order dynamical systems (Qt-based)',
-        packages=find_packages(),
-        include_package_data=True,
-        install_requires=requirements,
-        entry_points={'console_scripts': ['pyplane=pyplane.app:run']},
-        data_files=[]
-    )
+setup(
+    name='PyPlane',
+    version='2.0.0',
+    license='GPLv3',
+    url='https://github.com/TUD-RST/pyplane',
+    author='Klemens Fritzsche, Jan Winkler, Carsten Knoll',
+    author_email='firstname.lastname@tu-dresden.de',
+    description='Tool for phase plane analysis of second order dynamical systems (Qt-based)',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    entry_points={'console_scripts': ['pyplane=pyplane.app:run']},
+    data_file = [
+    ]
+)
